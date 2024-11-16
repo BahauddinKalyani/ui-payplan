3c48015d88704d5248f3e0e93f31cdda5689ec10
Bahauddin Kalyani <bahauddinkalyani@gmail.com>
2024-11-15 21:07:34 -0500

    change base url name.

--- src/api/transactionAPI.js
+++ src/api/transactionAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const transactionAPI = {
     get_user_transactions: async (user_id) => {
